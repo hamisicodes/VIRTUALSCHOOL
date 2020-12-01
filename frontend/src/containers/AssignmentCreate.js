6066485542253360153922fcc7fce5f7dc260408
ow-tech <wagwanwekon@outlook.com>
2020-12-01 15:46:28 +0300

    bug in code, assignment create and questionform; leaving for future deburging

--- frontend/src/containers/AssignmentCreate.js
+++ frontend/src/containers/AssignmentCreate.js
@@ -1,89 +1,119 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Form, Input, Button } from 'antd';
 import { MinusCircleOutlined, PlusSquareFilled } from '@ant-design/icons';
+import QuestionForm from './QuestionForm'
 
+const GroupedAssignmentCreate = (props) => {
+    const onFinish = values => {
+      console.log('Received values of form:', values);
+    };
+    const [ formCount, setFormCount] = useState(1)
 
-const GroupedAssignmentCreate = () => {
-  const onFinish = values => {
-    console.log('Received values of form:', values);
-  };
+    const questions = [];
 
-  return (
-    <Form name="dynamic_form_item" onFinish={onFinish} >
-        <h1 style = {{textAlign:"center"}}>Create An Assignment</h1>
-        {/* <Form.Item 
-            required={false}
-            
-        > */}
-            <Form.Item
+    for(let i =0; i < 4; i++){
+
+        questions.push(
+            <QuestionForm id={i} key={i} {...props} />
+        )
+    }
+    return (
+      <Form name="dynamic_form_item" onFinish=  {onFinish}>
+
+          {/* form item to handle assignment tittle */}
+
+          <h1 style = {{textAlign:"center"}}>Create Assignment</h1>
+        <Form.Item         
             style={{textAlign:"center"}}
-                label ={"Title: "}
-                validateTrigger={['onChange', 'onBlur']}
-                rules={[
-                {
-                    required :true,
-                    message: "Please input a title this Assignment(Hint: Supposed to be related with the Course/Subject/Module/Topic)!"
-                },
-                ]}>
-                <Input placeholder="Add Assignment title" required style={{ width: '40%', textAlign:"center" }} />
-            </Form.Item>  
-      <Form.List
-        name="names"
+            label ={"Title: "}
+            validateTrigger={['onChange', 'onBlur']}
+            rules={[
+            {
+                required :true,
+                message: "Please input a title this Assignment(Hint: Supposed to be related with the Course/Subject/Module/Topic)!"
+            },
+            ]}>
+            <Input placeholder="Add Assignment title" required style={{ width: '40%', textAlign:"center" }} />
+        </Form.Item>
         
-      >
-        {(fields, { add, remove }, { errors }) => (
-          <>
-            {fields.map((field, index) => (
-              <Form.Item
-                // label={index === 0 ? 'Questions' : ''}
-                required={false}
-                key={field.key}
-                style = {{textAlign:'center'}}
-              >
+        <Form.List
+          name="names"
+          rules={[
+            {
+              validator: async (_, names) => {
+                if (!names || names.length < 2) {
+                  return Promise.reject(new Error('At least 2 Questions'));
+                }
+              },
+            },
+          ]}
+        >
+          {(fields, { add, remove }, { errors }) => (
+            <>
+              {fields.map((question, index) => (
                 <Form.Item
-                  {...field}
-                  validateTrigger={['onChange', 'onBlur']}
-                  rules={[
-                    {
-                      required: true,
-                      whitespace: true,
-                      message: "Please input Question's Statement or delete this field.",
-                    },
-                  ]}
-                  noStyle
+                style={{textAlign:"center"}}
+                  label={index === 0 ? 'Questions' : ''}
+                  required={false}
+                  key={question.key}
                 >
-                <Input placeholder="Question Statement" style={{ width: '60%', textAlign:"center" }} />  
-                </Form.Item>
-                {fields.length > 1 ? (
-                  <MinusCircleOutlined
-                    className="dynamic-delete-button"
-                    onClick={() => remove(field.name)}
-                  />
-                ) : null}
-              </Form.Item>
-            ))}
-            <Form.Item style ={{textAlign:"center"}}>
-              <Button
-                type="default"
-                onClick={() => add()}
-                style={{ width: '20%',  marginLeft: '200px'  }}
-                
-                icon={<PlusSquareFilled />}
-              >
-                Add Question field
-              </Button>
-              <Form.ErrorList errors={errors} />
-            </Form.Item>
-          </>
-        )}
-      </Form.List>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
-  );
-};
+                  <Form.Item
+                    {...question}
+                    validateTrigger={['onChange', 'onBlur']}
+                    rules={[
+                      {
+                          
+                        required: true,
+                        whitespace: true,
+                        message: "Please input Question's Statement or delete this field.",
+                      },
+                    ]}
+                    noStyle
+                  >
+                    <Input placeholder="Question Statement" style={{ width: '60%' }} />
+                  </Form.Item>
+                  <Form.Item>
+                      {/* input of answer to the question */}
 
+                    <Form.Item>
+                        <Input placeholder="Whats The Answer" style={{ width: '25%' }} />
+                    </Form.Item>
+                  {questions}
+                  </Form.Item>
+                  {formCount > 1 ? (
+                    <MinusCircleOutlined
+                      className="dynamic-delete-button"
+                      onClick={() => remove(
+                          question.name,
+                          setFormCount(formCount - 1)
+                          )}
+                    />
+                  ) : null}
+                </Form.Item>
+              ))}
+             
+              <Form.Item>
+                <Button
+                  type="dashed"
+                  onClick={() => add(
+                    setFormCount(formCount + 1)
+                  )}
+                  style={{ width: '60%' }}
+                  icon={<PlusSquareFilled />}
+                >
+                  Add Question
+                </Button>
+                <Form.ErrorList errors={errors} />
+              </Form.Item>
+            </>
+          )}
+        </Form.List>
+        <Form.Item>
+          <Button type="secondary" htmlType="submit" >
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  };
 export default GroupedAssignmentCreate;
--- frontend/src/containers/QuestionForm.js
+++ frontend/src/containers/QuestionForm.js
@@ -1,89 +1,35 @@
 import React from 'react';
-
-import React from 'react';
 import { Form, Input, Button } from 'antd';
-import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 
 
-const GroupedQuestionForm = () => {
+const GroupedQuestionForm = (props) => {
   const onFinish = values => {
     console.log('Received values of form:', values);
   };
-
+  console.log(props.id)
+  
   return (
-    <Form name="dynamic_form_item" onFinish={onFinish} >
-      <Form.List
-        name="names"
-        rules={[
-          {
-            validator: async (_, names) => {
-              if (!names || names.length < 2) {
-                return Promise.reject(new Error('At least 2 passengers'));
-              }
-            },
-          },
-        ]}
-      >
-        {(fields, { add, remove }, { errors }) => (
-          <>
-            {fields.map((field, index) => (
+
               <Form.Item
-                label={index === 0 ? 'Choices' : ''}
+                label={'Choice'}
                 required={false}
-                key={field.key}
-              >
-                <Form.Item
-                  {...field}
-                  validateTrigger={['onChange', 'onBlur']}
-                  rules={[
-                    {
-                      required: true,
-                      whitespace: true,
-                      message: "Please input a Choice or delete this field.",
-                    },
-                  ]}
-                  noStyle
-                >
-                  <Input placeholder="Choice" style={{ width: '60%' }} />
-                </Form.Item>
-                {fields.length > 1 ? (
-                  <MinusCircleOutlined
-                    className="dynamic-delete-button"
-                    onClick={() => remove(field.name)}
-                  />
-                ) : null}
+                key={props.id}
+                style = {{padding:'0px'}}
+              > 
+                  <Form.Item
+                    validateTrigger={['onChange', 'onBlur']}
+                    rules={[
+                      {
+                        required: true,
+                        whitespace: true,
+                        message: "Please input a Choice or delete this field.",
+                      },
+                    ]}
+                    noStyle
+                  >
+                      <Input placeholder="Choice" style={{ width: '25%' }} />
+                  </Form.Item>
               </Form.Item>
-            ))}
-            <Form.Item>
-               <Button
-                type="dashed"
-                onClick={() => add()}
-                style={{ width: '60%' }}
-                icon={<PlusOutlined />}
-              >
-                Add field
-              </Button> 
-              <Button
-                type="dashed"
-                onClick={() => {
-                  add('The head item', 0);
-                }}
-                style={{ width: '60%', marginTop: '20px' }}
-                icon={<PlusOutlined />}
-              >
-                Add field at head
-              </Button>
-              <Form.ErrorList errors={errors} />
-            </Form.Item>
-          </>
-        )}
-      </Form.List>
-      {/* <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item> */}
-    </Form>
   );
 };
 
