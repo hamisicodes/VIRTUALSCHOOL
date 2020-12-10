71deb99d7328442615657572a5b29fc24cc68f8d
ow-tech <wagwanwekon@outlook.com>
2020-12-10 19:02:48 +0300

    function to handle the posting of the data

--- frontend/src/containers/AssignmentCreate.js
+++ frontend/src/containers/AssignmentCreate.js
@@ -1,31 +1,56 @@
 import React, {useEffect, useState} from 'react';
-import { Form, Input, Button , Space, Row} from 'antd';
+import { Form, Input, Button , Space, message, Row,} from 'antd';
 import { MinusCircleOutlined, PlusSquareFilled } from '@ant-design/icons';
-import QuestionForm from './QuestionForm'
 
 const GroupedAssignmentCreate = (props) => {
-	// const [formLayout, setFormLayout] = useState('horizontal');
-	// const [choiceValue, setChoiceValue]=useState('')
-	// const [ formCount, setFormCount] = useState(1)
 	const { TextArea } = Input;
-
+	// const [loading, setLoading]= useState(false)
+    const [error, setError] = useState(null)
+	// Creating an Assignment object that has Assignment and question(s)
     const onFinish = values => {
-		console.log('Received values of form:', values);
-		// console.log('Choices here i am',props.values)
+		// console.log('Received values of form:', values);
+		const questions = [];
+		for (let i =0; i < values.Question.length; i++){
+			questions.push({
+			title:values.Question[i].QuestionStatement,
+			choices:[values.Question[i].ChoiceA, values.Question[i].ChoiceB, values.Question[i].ChoiceC, values.Question[i].ChoiceD],
+			answer : values.Question[i].Answer
+				
+			});
+		}
+		const AssignmentData = {
+			educator :"educator_testing hardcorded",
+			title: values.assignmentTitle,
+			questions
+		}
+		message.success('Submiting Assignment Completed!');
+        createAssignment(AssignmentData)
+		// console.log("i am assignment detail",AssignmentData)
 	};
-
-	const onValuesChange = (values, valuesC) =>{
-		console.log('Reve', valuesC)
-	}
-
-
+	// Creating the assignment object in backend
+	function createAssignment(opts){
+        fetch('http://127.0.0.1:8000/api/assignments/', {
+            method:'post',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify(opts)
+        }).then(function(res){
+            return res.json();
+        })
+        .catch(error =>{
+            setError(error)
+        });
+        
+    }
     return (
 		<Form name="dynamic_form_nest_item" scrollToFirstError={true}	style = {{
 			textAlign: "center"
-		}}onFinish={onFinish} onValuesChange ={onValuesChange} autoComplete="off">
+		}}onFinish={onFinish}  autoComplete="off">
 			<Form.Item >
 				<Row type="flex" justify="center" align="middle" style={{minHeight: '10vh'}}>
-					<Form.Item name="Assignment" fieldKey="ass"
+					<Form.Item name="assignmentTitle" fieldKey="ass"
 						style = {{
 							textAlign:"center",
 							width:"550px",
@@ -56,7 +81,7 @@ const GroupedAssignmentCreate = (props) => {
 						fieldKey={[field.fieldKey, 'QuestionStatementId']}
 						rules={[{ required: true, message: 'Missing first name' }]}
 					>
-						<TextArea placeholder="QuestionStatement" autoSize={{ minRows: 2, maxRows: 6 }}/>
+						<TextArea placeholder="QuestionStatement" autoSize={{ minRows: 1, maxRows: 10 }}/>
 					</Form.Item>
 
 					{/* Choices form item */}
@@ -68,7 +93,7 @@ const GroupedAssignmentCreate = (props) => {
 							fieldKey={[field.fieldKey, '1']}
 						rules={[{ required: true, message: 'Missing first name' }]}
 					>
-						<TextArea placeholder="choice A" />
+						<TextArea placeholder="choice A" autoSize={{ minRows: 1, maxRows: 10 }}/>
 					</Form.Item>
 					<Form.Item
 						{...field} 
@@ -77,7 +102,7 @@ const GroupedAssignmentCreate = (props) => {
 							fieldKey={[field.fieldKey, '2']}
 						rules={[{ required: true, message: 'Missing first name' }]}
 					>
-						<TextArea placeholder="choice B" />
+						<TextArea placeholder="choice B" autoSize={{ minRows: 1, maxRows: 10 }}/>
 					</Form.Item>
 					<Form.Item
 						{...field} 
@@ -86,7 +111,7 @@ const GroupedAssignmentCreate = (props) => {
 							fieldKey={[field.fieldKey, '3']}
 						rules={[{ required: true, message: 'Missing first name' }]}
 					>
-						<TextArea placeholder="choice C" />
+						<TextArea placeholder="choice C" autoSize={{ minRows: 1, maxRows: 10 }}/>
 					</Form.Item>
 					<Form.Item
 						{...field} 
@@ -95,7 +120,7 @@ const GroupedAssignmentCreate = (props) => {
 							fieldKey={[field.fieldKey, '4']}
 						rules={[{ required: true, message: 'Missing first name' }]}
 					>
-						<TextArea placeholder="choice D" />
+						<TextArea placeholder="choice D" autoSize={{ minRows: 1, maxRows: 10 }}/>
 					</Form.Item>
 					{/* Answer item */}
 					<Form.Item
@@ -105,7 +130,7 @@ const GroupedAssignmentCreate = (props) => {
 							fieldKey={[field.fieldKey, '5']}
 						rules={[{ required: true, message: 'Missing first name' }]}
 					>
-						<TextArea placeholder="What's Answer" />
+						<TextArea placeholder="What's Answer" autoSize={{ minRows: 1, maxRows: 10 }}/>
 					</Form.Item>
 					{/* <QuestionForm 
 					{...field}
@@ -116,7 +141,7 @@ const GroupedAssignmentCreate = (props) => {
 					</Space>
 				))}
 				<Form.Item>
-					<Button type="dashed" style={{ width: '20%' }} onClick={() => add()} block icon={<PlusSquareFilled/>}>
+					<Button type="dashed" style={{ width: '25%' }} onClick={() => add()} block icon={<PlusSquareFilled/>}>
 					Add Question
 					</Button>
 				</Form.Item>
