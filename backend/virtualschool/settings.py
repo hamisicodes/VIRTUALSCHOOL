eabfa9ccdff617175c1e034b05a73bfe410ca3c9
hamisicodes <newhamisi@gmail.com>
2020-12-28 10:00:18 +0300

    merged changes into development

--- backend/virtualschool/settings.py
+++ backend/virtualschool/settings.py
@@ -77,7 +77,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
 
-        'DIRS': [os.path.join('templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
 
         'APP_DIRS': True,
         'OPTIONS': {
