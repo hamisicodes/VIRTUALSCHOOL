08855fd98da92c48880c87d65b67d77677f17045
hamisicodes <newhamisi@gmail.com>
2021-01-14 00:08:04 +0300

    ready to deploy

---
+++ build/precache-manifest.3f8b3f70ab560ec5eb4536a9ff6b27a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b404ebd612ff77587377fa5e16e1dfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64fce58b3e0145dced1b",
+    "url": "/static/css/2.00a338ba.chunk.css"
+  },
+  {
+    "revision": "4f75a3b2804a1b490717",
+    "url": "/static/css/main.27f190fb.chunk.css"
+  },
+  {
+    "revision": "64fce58b3e0145dced1b",
+    "url": "/static/js/2.0ccc9986.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/static/js/2.0ccc9986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f75a3b2804a1b490717",
+    "url": "/static/js/main.3ef1665f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "078ba7a3ea3b6469b4ec87b46ec495ae",
+    "url": "/static/media/antd.078ba7a3.less"
+  }
+]);
