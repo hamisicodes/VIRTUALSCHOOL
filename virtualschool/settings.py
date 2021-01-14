b740f88b7c0fbec385e308853aeee9967ba3fcd2
hamisicodes <newhamisi@gmail.com>
2021-01-15 00:37:05 +0300

    deployment configurations

--- virtualschool/settings.py
+++ virtualschool/settings.py
@@ -105,6 +105,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
         'TEST': {
             'NAME': 'test_virtualschool'
         }
